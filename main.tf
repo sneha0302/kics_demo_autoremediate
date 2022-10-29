269ee8dc3de2f2d0be632841445b9b777ccab92b
sneha0302 <77618131+sneha0302@users.noreply.github.com>
2022-10-30 02:03:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,48 +1,121 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 3.11"
-    }
+resource "aws_db_subnet_group" "km_rds_subnet_grp" {
+  name       = "km_rds_subnet_grp_${var.environment}"
+  subnet_ids = var.private_subnet
+
+  tags = merge(var.default_tags, {
+    Name = "km_rds_subnet_grp_${var.environment}"
+  })
+}
+
+resource "aws_security_group" "km_rds_sg" {
+  name   = "km_rds_sg"
+  vpc_id = var.vpc_id
+
+  tags = merge(var.default_tags, {
+    Name = "km_rds_sg_${var.environment}"
+  })
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-provider "aws" {
-  region = var.region
+resource "aws_kms_key" "km_db_kms_key" {
+  description             = "KMS Key for DB instance ${var.environment}"
+  deletion_window_in_days = 10
+  enable_key_rotation     = false
+
+  tags = merge(var.default_tags, {
+    Name = "km_db_kms_key_${var.environment}"
+  })
 }
 
-module "network" {
-  source       = "./modules/network"
-  environment  = var.environment
-  default_tags = var.default_tags
+resource "aws_db_instance" "km_db" {
+  name                      = "km_db_${var.environment}"
+  allocated_storage         = 20
+  engine                    = "postgres"
+  engine_version            = "10.6"
+  instance_class            = "db.t3.medium"
+  storage_type              = "gp2"
+  password                  = var.db_password
+  username                  = var.db_username
+  vpc_security_group_ids    = [aws_security_group.km_rds_sg.id]
+  db_subnet_group_name      = aws_db_subnet_group.km_rds_subnet_grp.id
+  identifier                = "km-db-${var.environment}"
+  storage_encrypted         = true
+  skip_final_snapshot       = true
+  final_snapshot_identifier = "km-db-${var.environment}-db-destroy-snapshot"
+  kms_key_id                = aws_kms_key.km_db_kms_key.arn
+  tags = merge(var.default_tags, {
+    Name = "km_db_${var.environment}"
+  })
 }
 
-module "storage" {
-  source         = "./modules/storage"
-  private_subnet = module.network.private_subnet
-  vpc_id         = module.network.vpc_id
-  environment    = var.environment
-  default_tags   = var.default_tags
-  db_username    = var.db_username
-  db_password    = var.db_password
+resource "aws_ssm_parameter" "km_ssm_db_host" {
+  name        = "/km-${var.environment}/DB_HOST"
+  description = "Kai Monkey Database"
+  type        = "SecureString"
+  value       = aws_db_instance.km_db.endpoint
+
+  tags = merge(var.default_tags, {})
 }
 
-module "compute" {
-  source = "./modules/compute"
-  environment = var.environment
-  region = var.region
-  elb_target_group_arn = module.network.target_lb_group_arn
-  private_subnet = module.network.private_subnet
-  public_subnet = module.network.public_subnet
-  elb_sg = module.network.target_lb_security_group
-  elb_url = module.network.elb_url
+resource "aws_ssm_parameter" "km_ssm_db_password" {
+  name        = "/km-${var.environment}/DB_PASSWORD"
+  description = "Kai Monkey Database Password"
+  type        = "SecureString"
+  value       = aws_db_instance.km_db.password
+
+  tags = merge(var.default_tags, {})
 }
 
-resource "local_file" "web-access" {
-  content  = <<JSON
-{
-  "fqdn": "${module.network.elb_url}"
+resource "aws_ssm_parameter" "km_ssm_db_user" {
+  name        = "/km-${var.environment}/DB_USER"
+  description = "Kai Monkey Database Username"
+  type        = "SecureString"
+  value       = aws_db_instance.km_db.username
+
+  tags = merge(var.default_tags, {})
 }
-  JSON
-  filename = "./web-access.json"
+
+resource "aws_ssm_parameter" "km_ssm_db_name" {
+  name        = "/km-${var.environment}/DB_NAME"
+  description = "Kai Monkey Database Name"
+  type        = "SecureString"
+  value       = aws_db_instance.km_db.name
+
+  tags = merge(var.default_tags, {
+    environment = "${var.environment}"
+  })
+}
+
+resource "aws_s3_bucket" "km_blob_storage" {
+  bucket = "km-blob-storage-${var.environment}"
+  acl    = "private"
+  tags = merge(var.default_tags, {
+    name = "km_blob_storage_${var.environment}"
+  })
+}
+
+resource "aws_s3_bucket" "km_public_blob" {
+  bucket = "km-public-blob"
+}
+
+resource "aws_s3_bucket_public_access_block" "km_public_blob" {
+  bucket = aws_s3_bucket.km_public_blob.id
+
+  block_public_acls   = false
+  block_public_policy = false
 }
